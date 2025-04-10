c5b4b7eca0e38cc010e5daf864b55c4bc0e71b87
Lu <navarreteluisa71@gmail.com>
2025-04-10 20:14:20 +0000

    lllllllllllllll

--- if-else.c
+++ if-else.c
@@ -11,4 +11,19 @@ int main(){
     } else {
         printf("%d is NOT equal to %d \n", num1, num2);
     }
+    //Exercise 2
+    int num3;
+    printf("enter one number: ");
+    scanf("%d", &num3);
+    if (num3%2 == 0) {
+        printf("number is even\n");
+    } else {
+        printf("number is odd\n");
+    }
+    //Exercise 3
+    int number1;
+    printf("enter 1 to 7 number");
+    scanf("%d", &number1);
+    if (number1 == )
+    return 0;
 }
